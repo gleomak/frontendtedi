9416edcfae170b235c0265decc4a6eb564e479eb
gleomak <sdi1900105@di.uoa.gr>
2023-09-03 20:41:52 +0300

    Download

--- src/features/ControlPanel/ControlPanel.tsx
+++ src/features/ControlPanel/ControlPanel.tsx
@@ -1,9 +1,18 @@
 import Typography from "@mui/material/Typography";
 import { User } from "../../app/models/user";
 import {useEffect, useState} from "react";
+import {useDispatch} from "react-redux";
 
 export default function ControlPanel(){
     const [users, setUsers] = useState<User[]>([]);
+    const [areUsersLoaded, setAreUsersLoaded] = useState<boolean>(false);
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        if(!areUsersLoaded){
+
+        }
+    })
 
     return(
         <Typography variant = "h2">
